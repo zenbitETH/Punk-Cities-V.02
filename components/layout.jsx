0e601c464924e2923168dfcbd381ea87bb181daa
HabacucMX <habacuc.vera@gmail.com>
2022-05-31 14:25:21 -0500

    Top-hud, left-hud and right-hud

---
+++ components/layout.jsx
@@ -0,0 +1,52 @@
+import Link from 'next/link';
+
+const Layout = ({ children, home }) => {
+  
+  return (
+    <div class="relative py-10 h-screen overflow-y-auto">
+      <nav class="top-hud">
+      <div class="hud-wallet">
+        <div>
+            Conectar
+        </div>
+      </div>
+      <div class="hud1">
+        <span> ⛲ </span>
+        <span class="mx-2">20</span>
+      </div>
+      <div class="hud2">
+        <span> ⚡ </span>
+        <span class="mx-2">20</span>
+      </div>
+      <div class="hud3">
+        <span> 💽 </span>
+        <span class="mx-2">12</span>
+      </div>
+    </nav>
+
+      <main>{children}</main>
+
+    <nav className="left-hud">
+        <Link href={``}>
+          <a className="huda"><div className='bigIcon'>⛲</div> Nuevo Lugar</a>
+        </Link>
+
+        <Link href={``}>
+          <a className="hudb"><div className='bigIcon'>🌇 </div>Mi ciudad</a>
+        </Link>
+    </nav>
+    <nav className='right-hud'>
+        <Link href={``}>
+          <a className="hudc"><div className='bigIcon'>🌎</div> Explorar</a>
+        </Link>
+    
+        <Link href={``}>
+          <a className="hudd"><div className='bigIcon'>🌞</div> Ciudad DAO</a>
+        </Link>
+    </nav>         
+   
+    </div>
+  );
+};
+
+export default Layout;
