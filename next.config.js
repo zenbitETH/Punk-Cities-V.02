aa9c82497c31e1753db86b3677d4b619a83b0ae1
HabacucMX <habacuc.vera@gmail.com>
2022-06-06 19:18:29 -0500

    env in next config

--- next.config.js
+++ next.config.js
@@ -4,3 +4,9 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+module.exports = {
+  env: {
+    NEXT_PUBLIC_NFT_TOKEN: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDY2YWRlY2E0MjNlYjlEODdCRTkxY2EzYzFlMzI3NTBkNzIxQTA5QTUiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY0OTUyODc4MDkwMCwibmFtZSI6IlNvbGFycHVuayBEQU8ifQ.DxHGpHTHCpkpQhCihCmc86r0i8x84XranbEhnKRBgWQ',
+  },
+}
